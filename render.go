49a72bdd879ab98a209b939b426f47d72f2242d3
Yu <benjamyu@amazon.com>
2018-10-21 22:42:22 -0400

    Basic sphere collision

--- render.go
+++ render.go
@@ -9,7 +9,19 @@ import (
 	"os"
 )
 
+func HitSphere(center primitives.Vector, radius float64, r primitives.Ray) bool {
+    oc := r.Origin().Sub(center)
+    a := r.Direction().Dot(r.Direction())
+    b := oc.Dot(r.Direction()) * 2.0
+    c := oc.Dot(oc) - (radius * radius)
+    discriminant := (b * b) - (4 * a * c)
+    return discriminant > 0
+}
+
 func Color(r primitives.Ray) primitives.Vector {
+    if HitSphere(primitives.Vector{0.0, 0.0, -1.0}, 0.5, r) {
+        return primitives.Vector{1.0, 0.0, 0.0}
+    }
 	unitDirection := r.Direction().Unit()
 	t := 0.5 * (unitDirection.Y() + 1.0)
 	return primitives.Vector{1.0, 1.0, 1.0}.ScalarMul(1.0 - t).Add(primitives.Vector{0.5, 0.7, 1.0}).ScalarMul(t)
@@ -25,17 +37,18 @@ func main() {
 	vertical := primitives.Vector{0.0, 2.0, 0.0}
 	origin := primitives.Vector{0.0, 0.0, 0.0}
 
-	for y := 0; y < height; y++ {
+	for y := height - 1; y >= 0; y-- {
 		for x := 0; x < width; x++ {
 			u := float64(x) / float64(width)
 			v := float64(y) / float64(height)
 			r := primitives.Ray{origin, lowerLeftCorner.Add(horizontal.ScalarMul(u)).Add(vertical.ScalarMul(v))}
 			col := Color(r)
 
+
 			img.Set(x, height-y, color.NRGBA{
-				R: uint8(col.R() * 255.9),
-				G: uint8(col.G() * 255.9),
-				B: uint8(col.B() * 255.9),
+				R: uint8(col.R() * 255.99),
+				G: uint8(col.G() * 255.99),
+				B: uint8(col.B() * 255.99),
 				A: 255,
 			})
 		}
